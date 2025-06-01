997da9b5420de304f40bb36fb35c64d4e4c0703b
ankitalodha05 <76703320+ankitalodha05@users.noreply.github.com>
2025-06-01 21:54:28 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -2,11 +2,31 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
 
+data "aws_availability_zones" "available" {}
+
 resource "aws_subnet" "subnets" {
   count             = length(var.subnet_cidrs)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.subnet_cidrs[count.index]
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
 }
 
-data "aws_availability_zones" "available" {}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_assoc" {
+  count          = length(var.subnet_cidrs)
+  subnet_id      = aws_subnet.subnets[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
