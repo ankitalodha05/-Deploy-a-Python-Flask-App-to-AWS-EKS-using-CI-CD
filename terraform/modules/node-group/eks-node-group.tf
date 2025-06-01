59513147652709c94bcf590b8b2cceeaf4a81d59
ankitalodha05 <76703320+ankitalodha05@users.noreply.github.com>
2025-06-01 21:03:15 +0530

    Update eks-node-group.tf

--- terraform/modules/node-group/eks-node-group.tf
+++ terraform/modules/node-group/eks-node-group.tf
@@ -4,7 +4,6 @@ module "eks_managed_node_group" {
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
-  vpc_id          = var.vpc_id
   subnet_ids      = var.subnet_ids
 
   name            = "flask-node-group"
