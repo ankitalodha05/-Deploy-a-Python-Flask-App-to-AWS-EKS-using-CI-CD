6fafbab6639f830097c0cfcd8852cd40ffdec334
ankitalodha05 <76703320+ankitalodha05@users.noreply.github.com>
2025-06-01 20:52:16 +0530

    Create variables.tf

---
+++ terraform/modules/node-group/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {}
+variable "cluster_version" {}
+variable "vpc_id" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "desired_size" {}
+variable "min_size" {}
+variable "max_size" {}
+
+variable "instance_types" {
+  type = list(string)
+}
+variable "capacity_type" {}
+variable "ami_type" {}
+
+variable "create_iam_role" {}
+variable "iam_role_name" {}
